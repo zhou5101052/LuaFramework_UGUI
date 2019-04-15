d208a4cb97828f7362b816a751ea2614111629a9
zhousilai <zhousilai@cookygame.com>
2019-04-15 12:36:26 +0800

    导入到2017.4.2自动升级api

--- Assets/LuaFramework/Scripts/Manager/PanelManager.cs
+++ Assets/LuaFramework/Scripts/Manager/PanelManager.cs
@@ -19,13 +19,13 @@ namespace LuaFramework {
         }
 
         /// <summary>
-        /// ´´½¨Ãæ°å£¬ÇëÇó×ÊÔ´¹ÜÀíÆ÷
+        /// ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½å£¬ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ô´ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
         /// </summary>
         /// <param name="type"></param>
         public void CreatePanel(string name, LuaFunction func = null) {
             string assetName = name + "Panel";
             string abName = name.ToLower() + AppConst.ExtName;
-            if (Parent.FindChild(name) != null) return;
+            if (Parent.Find(name) != null) return;
 
 #if ASYNC_MODE
             ResManager.LoadPrefab(abName, assetName, delegate(UnityEngine.Object[] objs) {
@@ -62,12 +62,12 @@ namespace LuaFramework {
         }
 
         /// <summary>
-        /// ¹Ø±ÕÃæ°å
+        /// ï¿½Ø±ï¿½ï¿½ï¿½ï¿½
         /// </summary>
         /// <param name="name"></param>
         public void ClosePanel(string name) {
             var panelName = name + "Panel";
-            var panelObj = Parent.FindChild(panelName);
+            var panelObj = Parent.Find(panelName);
             if (panelObj == null) return;
             Destroy(panelObj.gameObject);
         }
